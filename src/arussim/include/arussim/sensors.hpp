935555f0829d79bdc428d41dd93b976b255b60b7
RafaGuil <rafaguilvalero@gmail.com>
2024-10-12 14:39:35 +0200

    Remove: Unused dependencies

--- src/arussim/include/arussim/sensors.hpp
+++ src/arussim/include/arussim/sensors.hpp
@@ -1,6 +1,5 @@
 #include <rclcpp/rclcpp.hpp>
 #include "custom_msgs/msg/state.hpp"
-#include <geometry_msgs/msg/transform_stamped.hpp>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
