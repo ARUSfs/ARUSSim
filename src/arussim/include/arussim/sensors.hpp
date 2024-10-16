dae06effd1360e379f0d5fb0b0f193e67a7d1922
RafaGuil <rafaguilvalero@gmail.com>
2024-10-16 16:06:20 +0200

    Revert "Fix: Imu issue"
    This reverts commit 176fba36ede3cedd0c4ba56a9c5177bfa4a3ce92.

--- src/arussim/include/arussim/sensors.hpp
+++ src/arussim/include/arussim/sensors.hpp
@@ -35,12 +35,10 @@ private:
     double yaw_ = 0;
     double vx_ = 0;
     double vy_ = 0;
-    double ax_ = 0;
-    double ay_ = 0;
     double r_ = 0;
 
-    double noisy_ax_ = 0;
-    double noisy_ay_ = 0;
+    double noisy_vx_ = 0;
+    double noisy_vy_ = 0;
     double noisy_r_ = 0;
     double noisy_yaw_ = 0;
     double noisy_delta_ = 0;
@@ -64,8 +62,8 @@ private:
     double kNoiseImuX;
     double kNoiseImuY;
     double kNoiseImuYaw;
-    double kNoiseImuAx;
-    double kNoiseImuAy;
+    double kNoiseImuVx;
+    double kNoiseImuVy;
     double kNoiseImuR;
 
     /**
