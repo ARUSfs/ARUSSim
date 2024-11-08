f0629184cdb2319fccab40050b595e72ab0c5a72
alvarolp02 <alplepe02@gmail.com>
2024-11-08 02:02:24 +0100

    feat: simplify vehicle dynamics

--- src/arussim/include/arussim/vehicle_dynamics.hpp
+++ src/arussim/include/arussim/vehicle_dynamics.hpp
@@ -5,11 +5,23 @@ class VehicleDynamics
 {
     public:
         VehicleDynamics();
-        void update_simulation(double& x, double& y, double& yaw, double& vx, 
-                                double input_delta, double input_acc, double dt,
-                                double kMass, double kWheelBase);
+        void update_simulation(double input_delta, double input_acc, double dt);
+
+        double x_;
+        double y_;
+        double yaw_;
+        double vx_;
+        double vy_;
+        double r_;
+        double input_delta_;
+        double input_acc_;
+        double dt_;
     
     private:
+
+        double kMass = 270.0;
+        double kWheelBase = 1.535;
+
         double kMinFx = -3500;
         double kMaxFx = 2000;
 
@@ -19,7 +31,7 @@ class VehicleDynamics
 
         double x_dot_{0.0}, y_dot_{0.0}, yaw_dot_{0.0}, vx_dot_{0.0};
 
-        void calculate_dynamics(double vx, double yaw, double input_delta, double input_acc, double kMass, double kWheelBase);
-        void integrate_dynamics(double& x, double& y, double& yaw, double& vx, double dt);
-        double calculate_fx(double vx, double input_acc, double kMass);
+        void calculate_dynamics();
+        void integrate_dynamics();
+        double calculate_fx();
 };
