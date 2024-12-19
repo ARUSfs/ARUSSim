944d004fb3943a876f95bb3dc22cf98bd1f42d9b
Rafael Guil Valero <rafaguilvalero@gmail.com>
2024-12-19 10:29:04 +0100

    fix: launch format

--- src/arussim/launch/extended_arussim_launch.py
+++ src/arussim/launch/extended_arussim_launch.py
@@ -1,53 +1,42 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, DeclareLaunchArgument, RegisterEventHandler, Shutdown
+from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition
-from launch.event_handlers import OnProcessExit
 
 
 def generate_launch_description():    
+
     return LaunchDescription([
         create_node(pkg='rviz2',
                     exec='rviz2',
-                    config='arussim_rviz_config.rviz',
-                    config_pkg='arussim'),
-        create_node(exec='arussim',
-                    name='simulator',
+                    config='arussim_rviz_config.rviz'),
+        create_node(exec='arussim_exec',
                     config='simulator_config.yaml'),
-        create_node(exec='sensors',
+        create_node(exec='sensors_exec',
                     config='sensors_config.yaml'),
-        create_node(exec='supervisor'),
-        create_node(exec='extended_interface')
+        create_node(exec='supervisor_exec'),
+        create_node(exec='extended_interface_exec')
     ])
 
 
-def create_node(pkg='arussim', exec=None, params=[], name=None, config_pkg=None, config=None, condition=None): 
-    if config_pkg is None:
-        config_pkg = pkg
-    package_share_directory = get_package_share_directory(config_pkg)
-    if name != 'arussim':
-        name = exec
-    if exec != 'rviz2':
-        exec = exec + "_exec"
-    if config is not None:
-        config_file = os.path.join(package_share_directory, "config", config)
-    else:
-        config_file = None
-    if exec == 'rviz2':
-        parameters = params
-        arguments = ['-d', config_file]
-    else:
-        parameters = [config_file] + params if config_file else params
-        arguments = []
+def create_node(pkg='arussim', exec=None, params=[], config=None): 
+
+    if config is None:
+        config = pkg + "_config.yaml"
+    if exec is None:
+        exec = pkg + "_exec"
+
+    package_share_directory = get_package_share_directory('arussim')
+    config_file = os.path.join(package_share_directory, "config", config)
+
+    parameters = params if exec == 'rviz2' else [config_file] + params
+
     return Node(
         package=pkg,
         executable=exec,
-        name=name,
+        name=pkg,
         output="screen",
         parameters=parameters,
-        arguments=arguments,
-        condition=condition
+        arguments=['-d', config_file]
         )
