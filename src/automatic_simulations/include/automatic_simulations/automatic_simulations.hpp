31d7ae1c8077722c11c16d49d466771488df3cf4
Rafael Guil Valero <rafaguilvalero@gmail.com>
2024-12-28 14:52:24 +0100

    feat: reset algorithm

--- src/automatic_simulations/include/automatic_simulations/automatic_simulations.hpp
+++ src/automatic_simulations/include/automatic_simulations/automatic_simulations.hpp
@@ -6,7 +6,9 @@
  * @date 2024-12-28
  */
 #include <rclcpp/rclcpp.hpp>
-
+#include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include <string>
 
 /**
  * @class AutomaticSimulations
@@ -18,5 +20,19 @@ public:
     AutomaticSimulations();
 
 private:
+    std::string kEvent;
+    double kSimulationSpeedMultiplier;
+    double kIterations;
+    double kLapsTarget;
+
+    int current_laps_ = 0;
+    int current_iterations_ = 0;
+
+    void lap_time_callback(const std_msgs::msg::Float32::SharedPtr msg);
+
+
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr reset_pub_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr lap_time_sub_;
+    rclcpp::TimerBase::SharedPtr timer_;
 
 };
--- src/automatic_simulations/src/automatic_simulations.cpp
+++ src/automatic_simulations/src/automatic_simulations.cpp
@@ -7,7 +7,65 @@
  */
 AutomaticSimulations::AutomaticSimulations() : Node("automatic_simulations")
 {
+    // Declare parameters
+    this->declare_parameter<double>("simulation_speed_multiplier", 1.0);
+    this->declare_parameter<double>("iterations", 1.0);
+    this->declare_parameter<std::string>("event", "Trackdrive");
+    this->declare_parameter<double>("laps_target", 1.0);
 
+    // Get parameters
+    this->get_parameter("simulation_speed_multiplier", kSimulationSpeedMultiplier);
+    this->get_parameter("iterations", kIterations);
+    this->get_parameter("event", kEvent);
+    this->get_parameter("laps_target", kLapsTarget);
+
+    RCLCPP_INFO(this->get_logger(), "Starting automatic simulations for %s", kEvent.c_str());
+    RCLCPP_INFO(this->get_logger(), "Simulation speed multiplier: %f", kSimulationSpeedMultiplier);
+    RCLCPP_INFO(this->get_logger(), "Iterations: %f", kIterations);
+    RCLCPP_INFO(this->get_logger(), "Laps target: %f", kLapsTarget);
+
+    // Competition logic
+    if (kEvent == "Trackdrive"){
+        kLapsTarget = 10;
+    } else if (kEvent == "Acceleration"){
+        kLapsTarget = 1;
+    } else if (kEvent == "Skidpad"){
+        kLapsTarget = 4;
+    } else if (kEvent == "AutoX"){
+        kLapsTarget = 1;
+    }
+
+    // Publishers
+    reset_pub_ = this->create_publisher<std_msgs::msg::Bool>("/arussim/reset", 1);
+
+    // Subscribers
+    lap_time_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+        "/arussim/lap_time", 1, 
+        std::bind(&AutomaticSimulations::lap_time_callback, this, std::placeholders::_1)
+    );
+    
+}
+
+/**
+ * @brief Callback function for the lap time subscriber
+ * 
+ * @param msg 
+ */
+void AutomaticSimulations::lap_time_callback([[maybe_unused]]const std_msgs::msg::Float32::SharedPtr msg){
+    current_laps_ ++;
+    if (current_laps_ >= kLapsTarget){
+        auto message = std_msgs::msg::Bool();
+        message.data = true;
+        reset_pub_->publish(message);
+        current_iterations_++;
+        current_laps_ = 0;
+        RCLCPP_INFO(this->get_logger(), "Iteration %d completed", current_iterations_);
+    }
+    if (current_iterations_ >= kIterations){
+        RCLCPP_INFO(this->get_logger(), "All iterations completed");
+        rclcpp::shutdown();
+        exit(0);
+    }
 }
 
 /**
