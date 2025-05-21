dec7f51d2f726360aef8fc5ce21b253ee5c1e399
Jose Del Amor Gonzalez <79194446+josedag31@users.noreply.github.com>
2025-05-21 16:07:18 +0200

    Delete install directory

--- install/arussim_msgs/local/lib/python3.10/dist-packages/arussim_msgs/msg/__init__.py
+++
@@ -1,6 +0,0 @@
-from arussim_msgs.msg._cmd import Cmd  # noqa: F401
-from arussim_msgs.msg._cmd4_wd import Cmd4WD  # noqa: F401
-from arussim_msgs.msg._four_wheel_drive import FourWheelDrive  # noqa: F401
-from arussim_msgs.msg._point_xy import PointXY  # noqa: F401
-from arussim_msgs.msg._state import State  # noqa: F401
-from arussim_msgs.msg._trajectory import Trajectory  # noqa: F401
