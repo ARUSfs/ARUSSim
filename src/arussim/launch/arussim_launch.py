7851622b11ebc1f9ae74af86098416ece79049c6
Carmen Menendez <carmenmenendezda@gmail.com>
2025-10-25 16:01:58 +0200

    feat: create can

--- src/arussim/launch/arussim_launch.py
+++ src/arussim/launch/arussim_launch.py
@@ -1,7 +1,7 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, TimerAction
 from launch_ros.actions import Node
 
 
@@ -12,8 +12,45 @@ def generate_launch_description():
         'config',
         'arussim_rviz_config.rviz'
     )
+    modprobe_vcan = ExecuteProcess(
+        cmd=['sudo','modprobe', 'vcan'],
+        shell=False
+    )
+    create_can0 = TimerAction(
+    period=1.0, 
+    actions=[
+        ExecuteProcess(
+            cmd=['sudo','ip', 'link', 'add', 'dev', 'can0', 'type', 'vcan'],
+            shell=False
+        )
+    ]
+    )
+    up_can0 = TimerAction(
+        period=1.5,
+        actions=[
+            ExecuteProcess(
+                cmd=['sudo','ip', 'link', 'set', 'up', 'can0'],
+                shell=False
+            )
+        ]
+    )
+    create_can1 = ExecuteProcess(
+    cmd=['sudo','ip', 'link', 'add', 'dev', 'can1', 'type', 'vcan'],
+    shell=False
+    )
     
+    up_can1 = ExecuteProcess(
+    cmd=['sudo','ip', 'link', 'set', 'up', 'can1'],
+    shell=False
+    )  
+
+
     return LaunchDescription([
+        modprobe_vcan,
+        create_can0,
+        up_can0,
+        create_can1,
+        up_can1,
         Node(
             package='rviz2',
             executable='rviz2',
@@ -30,7 +67,7 @@ def generate_launch_description():
                     config='sensors_config.yaml'),
         create_node(pkg='arussim',
                     exec='supervisor_exec',
-                    config='supervisor_config.yaml')
+                    config='supervisor_config.yaml'),
     ])
 
 
