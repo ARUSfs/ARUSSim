fb79cd5163268fcfb0d4cc3b95a6e6384d0a0f59
FranciscoAlmeroRuiz <francisco.a.r.923@gmail.com>
2026-06-02 16:46:14 +0200

    fix: delete unused files and imports

--- src/arussim/launch/arussim_launch.py
+++ src/arussim/launch/arussim_launch.py
@@ -1,8 +1,6 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
